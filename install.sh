57e6657a7e0ac16e732521328fd8a517f96fb36a
SJCorp <sjcorp@users.noreply.github.com>
2020-09-26 14:36:32 +0530

    modded for RPi 3

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if [ -z "$1" ]; then
 fi
 
 if [ ! -e ngrok.service ]; then
-    git clone --depth=1 https://github.com/vincenthsu/systemd-ngrok.git
+    git clone --depth=1 https://github.com/sjcorp/systemd-ngrok.git
     cd systemd-ngrok
 fi
 cp ngrok.service /lib/systemd/system/
@@ -30,9 +30,9 @@ cp ngrok.yml /opt/ngrok
 sed -i "s/<add_your_token_here>/$1/g" /opt/ngrok/ngrok.yml
 
 cd /opt/ngrok
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok-stable-linux-amd64.zip
-rm ngrok-stable-linux-amd64.zip
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+rm ngrok-stable-linux-arm.zip
 chmod +x ngrok
 
 systemctl enable ngrok.service
